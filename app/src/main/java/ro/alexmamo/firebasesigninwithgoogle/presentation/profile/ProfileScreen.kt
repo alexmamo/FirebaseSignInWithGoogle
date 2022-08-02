de9a590d74375e2f686a39e113a9ba912dae1c35
Alex Mamo <alexmamo@gmail.com>
2022-08-02 14:53:04 +0300

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firebasesigninwithgoogle/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/ro/alexmamo/firebasesigninwithgoogle/presentation/profile/ProfileScreen.kt
@@ -4,15 +4,13 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarResult.ActionPerformed
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
-import ro.alexmamo.firebasesigninwithgoogle.components.ProgressBar
+import ro.alexmamo.firebasesigninwithgoogle.components.CheckRevokeAccessState
+import ro.alexmamo.firebasesigninwithgoogle.components.CheckSignOutState
 import ro.alexmamo.firebasesigninwithgoogle.core.Constants.REVOKE_ACCESS_MESSAGE
 import ro.alexmamo.firebasesigninwithgoogle.core.Constants.SIGN_OUT
-import ro.alexmamo.firebasesigninwithgoogle.core.Utils.Companion.print
-import ro.alexmamo.firebasesigninwithgoogle.domain.model.Response.*
 import ro.alexmamo.firebasesigninwithgoogle.presentation.auth.AuthViewModel
 import ro.alexmamo.firebasesigninwithgoogle.presentation.profile.components.ProfileContent
 import ro.alexmamo.firebasesigninwithgoogle.presentation.profile.components.ProfileTopBar
@@ -46,46 +44,24 @@ fun ProfileScreen(
         scaffoldState = scaffoldState
     )
 
-    when(val signOutResponse = viewModel.signOutResponse) {
-        is Loading -> ProgressBar()
-        is Success -> signOutResponse.data?.let { signedOut ->
-            if (signedOut) {
-                LaunchedEffect(signOutResponse.data) {
-                    navigateToAuthScreen()
+    CheckSignOutState(
+        signOutResponse = viewModel.signOutResponse,
+        navigateToAuthScreen = navigateToAuthScreen
+    )
+
+    CheckRevokeAccessState(
+        revokeAccessResponse = viewModel.revokeAccessResponse,
+        navigateToAuthScreen = navigateToAuthScreen,
+        showSnackBar = {
+            coroutineScope.launch {
+                val result = scaffoldState.snackbarHostState.showSnackbar(
+                    message = REVOKE_ACCESS_MESSAGE,
+                    actionLabel = SIGN_OUT
+                )
+                if (result == ActionPerformed) {
+                    viewModel.signOut()
                 }
             }
         }
-        is Error -> signOutResponse.e?.let {
-            LaunchedEffect(Unit) {
-                print(it)
-            }
-        }
-    }
-
-    fun showSnackBar() = coroutineScope.launch {
-        val result = scaffoldState.snackbarHostState.showSnackbar(
-            message = REVOKE_ACCESS_MESSAGE,
-            actionLabel = SIGN_OUT
-        )
-        if (result == ActionPerformed) {
-            viewModel.signOut()
-        }
-    }
-
-    when(val revokeAccessResponse = viewModel.revokeAccessResponse) {
-        is Loading -> ProgressBar()
-        is Success -> revokeAccessResponse.data?.let { accessRevoked ->
-            if (accessRevoked) {
-                LaunchedEffect(revokeAccessResponse.data) {
-                    navigateToAuthScreen()
-                }
-            }
-        }
-        is Error -> revokeAccessResponse.e?.let {
-            LaunchedEffect(Unit) {
-                print(it)
-                showSnackBar()
-            }
-        }
-    }
+    )
 }
